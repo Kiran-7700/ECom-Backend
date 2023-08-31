a6d48905c13b1f35d26fc53f0b60b650cfd7bda0
Kiran-7700 <121047254+Kiran-7700@users.noreply.github.com>
2023-08-31 08:17:15 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ const mongoose=require("mongoose");
 
 const connectDB=async ()=>{
     try{
-     const conn=await mongoose.connect("mongodb+srv://kiranchomule224:kiran7700@e-com.lhxgjau.mongodb.net/ecommerce")
+        const uri="mongodb+srv://kiranchomule224:kiran7700@e-com.lhxgjau.mongodb.net/ecommerce"
+     const conn=await mongoose.connect(uri)
      console.log(`connected to DB ${conn.connection.host}`);
     }catch(err){
         console.log(`Error connecting to database: ${err}`);
